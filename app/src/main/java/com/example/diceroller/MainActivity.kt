dc3d6c7127acb789d94215428b5c77e0ca1ab847
Farhan N <s.farhan3169@gmail.com>
2021-06-01 18:29:45 +0500

    Dice Roller App complete with images

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,11 +11,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // My Code starts here
         val rollButton: Button = findViewById(R.id.button_roll)
-        rollButton.setOnClickListener {
-            rollDice()
-            //val toast1 = Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT)
-            //toast1.show()
-        }
+        rollButton.setOnClickListener { rollDice() }
+
+        // Do a rollDice when the app starts
+        rollDice()
 
     }
 
@@ -23,17 +22,22 @@ class MainActivity : AppCompatActivity() {
         // Create a Dice object and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
 
-        val dice2 = Dice(6)
-        val diceRoll2 = dice2.roll()
-
-        // Update the screen with the value of the dice
-        val resultText: TextView = findViewById(R.id.textView)
-        resultText.text = diceRoll.toString()
-
-        val resultText2: TextView =  findViewById(R.id.textView2)
-        resultText2.text = diceRoll2.toString()
-
+        // select the image that corresponcds with the value of diceRoll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        // update the correct image to display
+        diceImage.setImageResource(drawableResource)
+        
+        // to facilitate screen reading, must give content description to images
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 // The Dice Class takes number of sides as argument
